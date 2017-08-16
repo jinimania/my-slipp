9d8713b70a8aac6a9a637eb6ba4eb6786b9d544d
ssoon <jjinimania@gmail.com>
2017-08-17 01:40:06 +0900

    리턴값 변경

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -29,7 +28,7 @@ public class UserController {
 
   @GetMapping("/form")
   public String form() {
-    return "/user/form";
+    return "user/form";
   }
 
   @PostMapping("")
@@ -43,7 +42,7 @@ public class UserController {
   @GetMapping("")
   public String list(final Model model) {
     model.addAttribute("users", userRepository.findAll());
-    return "/user/list";
+    return "user/list";
   }
 
   @GetMapping("/{id}/form")
