b1cfdf41c688618222ee11ee76c371c4f0591546
ssoon <jjinimania@gmail.com>
2017-08-17 01:16:32 +0900

    개인정보 수정 기능 구현 완료

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -2,26 +2,61 @@ package net.slipp.web;
 
 import java.util.ArrayList;
 import java.util.List;
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
   private List<User> users = new ArrayList<>();
 
-  @PostMapping("/create")
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserController(final UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping("/form")
+  public String form() {
+    return "/user/form";
+  }
+
+  @PostMapping("")
   public String create(final User user) {
     System.out.println("user: " + user);
     users.add(user);
-    return "redirect:/list";
+    userRepository.save(user);
+    return "redirect:/users";
   }
 
-  @GetMapping("/list")
+  @GetMapping("")
   public String list(final Model model) {
-    model.addAttribute("users", users);
-    return "list";
+    model.addAttribute("users", userRepository.findAll());
+    return "/user/list";
+  }
+
+  @GetMapping("/{id}/form")
+  public String updateForm(final @PathVariable Long id, final Model model) {
+    model.addAttribute("user", userRepository.findOne(id));
+    return "/user/updateForm";
+  }
+
+  @PostMapping("/{id}")
+  public String update(final @PathVariable Long id, final @ModelAttribute User newUser) {
+    final User user = userRepository.findOne(id);
+    user.update(newUser);
+    userRepository.save(user);
+    return "redirect:/users";
   }
 }
